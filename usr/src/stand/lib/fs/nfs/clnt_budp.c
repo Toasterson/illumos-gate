afa715fc97335f0cbb835a36e453368eb5488cb7
Hayashi Naoyuki <alpha@culzean.org>
2017-03-08 22:28:56 +0900

    fix multiple definition in stand (bootloader)

--- usr/src/stand/lib/fs/nfs/clnt_budp.c
+++ usr/src/stand/lib/fs/nfs/clnt_budp.c
@@ -23,6 +23,9 @@
  * Copyright 2006 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
+/*
+ * Copyright 2017 Hayashi Naoyuki
+ */
 
 /* Copyright (c) 1983, 1984, 1985, 1986, 1987, 1988, 1989 AT&T */
 /* All Rights Reserved */
